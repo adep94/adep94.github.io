d844b3644618aeec7fa52313d27bd610bba37d57
adep94 <andreadepisapia3@gmail.com>
2025-11-29 19:11:43 +0100

    added interactive backgr

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,59 @@
+/* ===============================================
+   PARTICLES BACKGROUND
+================================================ */
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": { "enable": true, "value_area": 900 }
+        },
+        "color": { "value": "#00e5ff" },
+        "shape": {
+            "type": "circle",
+            "stroke": { "width": 0, "color": "#000000" }
+        },
+        "opacity": {
+            "value": 0.3,
+            "random": true,
+            "anim": { "enable": false }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": { "enable": false }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#00e5ff",
+            "opacity": 0.25,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 1.2,
+            "direction": "none",
+            "out_mode": "out"
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": { "enable": true, "mode": "grab" },
+            "onclick": { "enable": true, "mode": "push" }
+        },
+        "modes": {
+            "grab": {
+                "distance": 180,
+                "line_linked": { "opacity": 0.35 }
+            },
+            "push": { "particles_nb": 4 }
+        }
+    },
+    "retina_detect": true
+});
+
+
 /* Scroll reveal */
 const reveals = document.querySelectorAll(".reveal");
 
